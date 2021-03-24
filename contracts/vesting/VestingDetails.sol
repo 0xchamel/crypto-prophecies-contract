d20d780270e02bcae6a200c5118fa386baa4deec
Sergey Sudakov <sergey95@gmail.com>
2021-03-24 11:57:08 +0000

    vesting getters

--- contracts/vesting/VestingDetails.sol
+++ contracts/vesting/VestingDetails.sol
@@ -5,12 +5,24 @@ import "./VestingStorage.sol";
 
 contract VestingDetails is VestingStorage, Ownable {
 
-  function isPaused(uint256 id) public view returns (bool) {
-    return investors[id].paused;
+  function getBeneficiary(uint256 id) public view returns (address) {
+    return investors[id].beneficiary;
   }
 
   function getCliff(uint256 id) public view returns (uint256) {
     return investors[id].cliff;
+  }
+
+  function getStart(uint256 id) public view returns (uint256) {
+    return investors[id].start;
+  }
+
+  function getDuration(uint256 id) public view returns (uint256) {
+    return investors[id].duration;
+  }
+
+  function isPaused(uint256 id) public view returns (bool) {
+    return investors[id].paused;
   }
 
 }
