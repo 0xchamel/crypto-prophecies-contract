ed4fad2a7a591a9e52c359cb29c698b9d35e7a0a
Dennis <itdev1210@gmail.com>
2021-05-10 05:26:53 -0400

    unit test for view

--- tests/nft/checkNFT.ts
+++ tests/nft/checkNFT.ts
@@ -15,7 +15,28 @@ export default async function suite() {
     it('Can mint correctly', async function () {
       //(uint8 generation, uint8 rarity, uint8 race, uint8 character
       let id = await this.nft._createProphet(1, 3, 2, 5, this.owner);
-    });
+      id = new BN(id.logs[0].args.tokenId);
+      id = id.toNumber();
 
+      const { generation, rarity, race, character } = await this.nft.getProphet(id)
+
+      expect(generation.toNumber()).to.equal(1);
+      expect(rarity.toNumber()).to.equal(3);
+      expect(race.toNumber()).to.equal(2);
+      expect(character.toNumber()).to.equal(5);
+    });
+    it('Can mint more', async function () {
+      //(uint8 generation, uint8 rarity, uint8 race, uint8 character
+      let id = await this.nft._createProphet(2, 2, 2, 2, this.owner);
+      id = new BN(id.logs[0].args.tokenId);
+      id = id.toNumber();
+
+      const { generation, rarity, race, character } = await this.nft.getProphet(id)
+
+      expect(generation.toNumber()).to.equal(2);
+      expect(rarity.toNumber()).to.equal(2);
+      expect(race.toNumber()).to.equal(2);
+      expect(character.toNumber()).to.equal(2);
+    });
   });
 }
