15b33db3af7a1f09e8de4758e4fac450b6598c3f
Sergey Sudakov <sergey95@gmail.com>
2021-05-30 01:26:36 +0100

    migration fix

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,4 +2,6 @@ const CryptoPropheciesToken = artifacts.require("TCP");
 
 module.exports = function (deployer, network, accounts) {
   deployer.deploy(CryptoPropheciesToken, "250000000000000000000000000");
+  let tcpInstance = await CryptoPropheciesToken.deployed();
+  await tcpInstance.mint();
 };
