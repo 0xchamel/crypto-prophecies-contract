2043135d628c53a8047e251419cd27369fb951db
Sergey Sudakov <sergey95@gmail.com>
2021-06-02 19:32:11 +0100

    more distribution stuff

--- migrations/3_prophet_spawn.js
+++ migrations/3_prophet_spawn.js
@@ -3,6 +3,7 @@ module.exports = async function (deployer, network, accounts) {
   let NFTinstance = await Prophet.deployed();
   console.log(NFTinstance.address);
   let addresses = [
+    //TEAM
     // "0xA5403cECD0F4Ffd25B5b86BCF1d2b8FD5CF7474d", //sergey
     // "0xC0e364A5D5b6080E35a86aa20F61509bc418c875", //dennis
     // "0x3e0f78886d062440DC8DBe6787Db302A0fE90341", //adam
@@ -10,7 +11,7 @@ module.exports = async function (deployer, network, accounts) {
     // "0x9dDf96E77cD37A1EE90e3934C615ab294328f23F", //Paul
     // "0xa07469269466bB05025DB99fDD79077347eb639c", //Phi
     // "0x9608d8154814b0124ceBbc29FdCbf51f4AbFa56A", //Gerald
-    // "0xb8C71f2aa276F243B4BcfABFb51e77F00bf32eA7 ", //Hamza
+    // "0xb8C71f2aa276F243B4BcfABFb51e77F00bf32eA7", //Hamza
     // "0x41120f8306ccFe31Fd3ab163090d97EE4260725B", //Stu
     // "0xa031F70EA3cA201194A7Fdeded5CEfc8cF739149", //Rini
     // "0x3e59A97DC8B5C0846E5a70562cb34a53b63f461a", //Ruth
@@ -18,9 +19,8 @@ module.exports = async function (deployer, network, accounts) {
     // "0xEeee1Aa700e714F974DBbA3bb610853AE8f2B803", //Sue
 
 
-
+    //FIRST SET
     // "0x56a512BC271E9cb638E480954e2321009A6D5f4F",
-    // "0xadda30c27f1e750ca8904dcfe2abba369808274b",
     // "0xd6C103d07697B4feCd4F104a131Ea9809Ec336fD",
     // "0xCe5600F9aC619677CA3E0853d1FdDd067919B3fd",
     // "0x2a8EefBDa9C321D577Fee6f3Db486919E26Cd14d",
@@ -40,16 +40,34 @@ module.exports = async function (deployer, network, accounts) {
     // "0x9a75d4691C90Affa38241Ed30CddD1aAba32C917",
     // "0xE27B5CD2C2402c072af2Dd58Ec0DcfE457630086",
     // "0x901c4A1A7a8f1c914F7E9856E634CC57b374f771",
+
+    //SECOND SET
+    // "0x8E8A0a425Bd00BE1cd29Ea374dC613292D496FD8",
+    // "0xadda30c27f1e750ca8904dcfe2abba369808274b",
+    // "0x99D1de1982C5C9834d7eFa5726460C8A092A88Cc",
+
+
+    //EXTRA
+    // "0x8BA29Acbc17D043635CD98ABD5ebA7adB2c665D4",
+    // "0xB800895394C34509978Af9Ee26A9B821Ac360C53",
   ];
   //let NFTinstance = Prophet.deployed();
   for (let address of addresses) {
     console.log("Address: " + address);
-    await NFTinstance._createProphet(1, 4, 0, 0, address);
-    await NFTinstance._createProphet(1, 4, 0, 1, address);
-    await NFTinstance._createProphet(1, 4, 0, 2, address);
-    await NFTinstance._createProphet(1, 4, 0, 3, address);
-    await NFTinstance._createProphet(1, 4, 0, 4, address);
-    await NFTinstance._createProphet(1, 4, 0, 5, address);
+
+    // FIRST SET
+    // await NFTinstance._createProphet(1, 4, 0, 0, address);
+    // await NFTinstance._createProphet(1, 4, 0, 1, address);
+    // await NFTinstance._createProphet(1, 4, 0, 2, address);
+    // await NFTinstance._createProphet(1, 4, 0, 3, address);
+    // await NFTinstance._createProphet(1, 4, 0, 4, address);
+    // await NFTinstance._createProphet(1, 4, 0, 5, address);
+
+    //SECOND SET
+    // await NFTinstance._createProphet(1, 4, 1, 0, address);
+    // await NFTinstance._createProphet(1, 4, 1, 1, address);
+    // await NFTinstance._createProphet(1, 4, 1, 2, address);
+
     //TODO RANDOMISE ITEMS
   }
   //_createProphet(uint16 generation, uint16 rarity, uint16 race, uint16 character, address destination)
